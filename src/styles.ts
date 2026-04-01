9d9168f7f5c0035381efc326e616f9c132175ee6
Graham Gear <graham.gear@nowhere.org>
2026-04-01 14:02:01 +0800

    feature: even more descriptors

--- src/styles.ts
+++ src/styles.ts
@@ -13,7 +13,7 @@ export default css`
 
   clock-weather-card-today-left {
     display: flex;
-    width: 35%;
+    width: 25%;
     align-items: center;
     justify-content: center;
   }
@@ -25,7 +25,7 @@ export default css`
 
   clock-weather-card-today-right {
     display: flex;
-    width: 65%;
+    width: 75%;
     justify-content: space-around;
     align-items: center;
   }
